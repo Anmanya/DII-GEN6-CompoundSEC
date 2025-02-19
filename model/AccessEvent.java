33c8abccbc39742607c1ba91d8bb20e01f5711da
Anmanya <Anmanya_khongmee@cmu.ac.th>
2025-02-20 00:38:25 +0700

    Inheritance

---
+++ model/AccessEvent.java
@@ -0,0 +1,24 @@
+package ggnamy.model;
+
+import java.time.LocalDateTime;
+
+
+public class AccessEvent {
+    private String user;
+    private String room;
+    private boolean accessGranted;
+    private LocalDateTime timestamp;
+
+    public AccessEvent(String user, String room, boolean accessGranted) {
+        this.user = user;
+        this.room = room;
+        this.accessGranted = accessGranted;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return timestamp + " | User: " + user + " | Room: " + room + " | Access: " + (accessGranted ? "GRANTED" : "DENIED");
+    }
+}
+
